93d896d8c0fc6307adc503bc6a1b478db96bbcc3
Anand <achaudharyskn@gmail.com>
2025-08-04 10:38:13 +0530

    class logic

---
+++ Class.logic/my-project/src/ERC20.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "forge-std/Test.sol";
+
+contract KiratCoin is ERC20 {
+    address stakingContract;
+    
+    constructor(address _stakingContract) ERC20("KiratCoin", "KIRA") {
+        stakingContract = _stakingContract;
+    }
+
+    modifier onlyContract() {
+        require(msg.sender == stakingContract);
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyContract {
+        _mint(to, amount);
+    }
+
+    function updateContract(address newContract) public onlyContract {
+        stakingContract = newContract;
+    }
+}
---
+++ Class.logic/my-project/test/ERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/ERC20.sol";
+
+contract ERC20ContractTest is Test {
+    KiratCoin c;
+
+    function setUp() public {
+        c = new KiratCoin(address(this));
+    }
+
+    function testMint() public {
+        uint value = 10;
+        c.mint(address(this), value);
+
+        assert(c.balanceOf(address(this)) == value);
+    }
+
+    function testFailMint() public {
+        uint value = 10;
+        vm.startPrank(0x587EFaEe4f308aB2795ca35A27Dff8c1dfAF9e3f);
+        c.mint(address(this), value);
+    }
+}
